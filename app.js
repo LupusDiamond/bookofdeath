9fd5b7998cd757d4da46dd3c0b2b57241f764337
LupusDiamond <godzplayer@hotmail.com>
2019-11-30 18:01:01 +0200

    dw arrow

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ function openCh() {
 }
 
 setTimeout(openCh, 900);
+
+// Next Chapter function
+const dwArrow = document.getElementById("dw-arr");
+
+function findNext() {
+  console.log(
+    parseInt(dwArrow.href.substr(dwArrow.href.length - 1).toString(10)) + 1
+  );
+  dwArrow.href =
+    "#" +
+    (
+      parseInt(dwArrow.href.substr(dwArrow.href.length - 1).toString(10)) + 1
+    ).toString(10);
+}
+
+function gotoCh(chapter) {
+  setTimeout(findNext, 50);
+}
